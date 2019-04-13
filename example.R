aff2c5f19742d32bb4aec4e808b644367803c4ad
Thevaa Chandereng <chandereng@wisc.edu>
2019-04-13 02:23:43 -0500

    updated example

--- example.R
+++ example.R
@@ -1,7 +1,12 @@
-# setting the seed
-set.seed(18990)
+## loading libarries 
+library(LPWC)
+library(ggplot2)
 
-## reading the dataset, please replace this link with your dataset
+# setting the seed
+set.seed(12229)
+
+## reading the dataset
+## please replace this link with your dataset
 data <- read.delim("http://www.cs.cmu.edu/~jernst/st/g27_1.txt", header = T)
 
 ## selecting columns with time series data
@@ -11,8 +16,19 @@ num.data <- data[, c(3:7)]
 num.data <- num.data[complete.cases(num.data), ]
 
 ## subset the data by randomly collecting 200 genes
-## ignore this line when analyzing your data, we subset the data to speed up the computation
-num.data <- num.data[sample(1:dim(num.data)[1], 200), ]
+## ignore this line when analyzing your data, we subset the data to 
+##speed up the computation
+num.data <- num.data[sample(1:dim(num.data)[1], 100), ]
+
+## analyzing time series data with 100 genes using LPWC with high penalty
+output.high <- corr.bestlag(data = num.data, timepoints = c(0.1, 0.5, 3, 6, 12))
+
+## analyzing time series data with 100 genes using LPWC with low penalty
+output.low <- corr.bestlag(data = num.data, timepoints = c(0.1, 0.5, 3, 6, 12), 
+                           penalty = "low")
+
+
+
 
 
 
